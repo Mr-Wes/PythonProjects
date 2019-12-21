eac76718b24334af97be9fa44bc66186277e567a
Mr-Wes <dmsmc@outlook.com>
2019-12-21 16:39:36 +0800

    201912211421

--- FileManager/src/__init__.py
+++ FileManager/src/__init__.py
@@ -1,6 +1,9 @@
+import os
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from src.Main import Ui_MainWindow
+from src.handleXML import writeRep, getRepsName, createXML
 
 
 class Main(QMainWindow, Ui_MainWindow):
@@ -8,6 +11,19 @@ class Main(QMainWindow, Ui_MainWindow):
         super(Main, self).__init__(parent)
         self.setupUi(self)
         self.setWindowTitle("W's File")
+        # 获取配置文件中所有仓库名
+        names = getRepsName()
+        # TODO 将 names 刷新 lv_reps
+
+    def slotSelectDir(self):
+        # 选择作为仓库的文件夹
+        dir = QFileDialog.getExistingDirectory()
+        dirname = os.path.basename(dir)
+        # 将文件夹路径及名称写入到配置文件
+        writeRep(dirname, dir)
+        # TODO 创建相应的 xml 文件
+        createXML(dirname, 'files')
+        # TODO 将文件夹名称传入 lv_reps,并刷新
 
 
 if __name__ == '__main__':
